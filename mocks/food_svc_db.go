6aaf35698049e595ad2ec52b309944d7a3969e06
Jacob Landowski <j.landowski@f5.com>
2019-07-13 15:40:59 -0700

    Create user and food db data

---
+++ mocks/food_svc_db.go
@@ -0,0 +1,24 @@
+package mocks
+
+import (
+	"github.com/angelhack2019/lib/models"
+)
+
+type Foods []models.Food
+
+// Food 1 : Created 7/13 expires 7/19
+// Food 2 : Created 7/5 expires 7/12
+var foods = Foods{
+	{
+		UUID:       "688c0daf-2833-485c-a419-b84a152a75a6",
+		PicURL:     "https://upload.wikimedia.org/wikipedia/commons/1/1d/Bagel-Plain-Alt.jpg",
+		CreateDate: 1563030000,
+		ExpDate:	1563494400,
+	},
+	{
+		UUID:       "688c0daf-2833-485c-a419-b84a152a75a6",
+		PicURL:     "https://www.impactcommunicationsinc.com/wp-content/uploads/2018/03/18-04_has_your_credibility_banana_turned_brown_1000.jpg",
+		CreateDate: 1562319325,
+		ExpDate:	1562889600,
+	},
+}
---
+++ mocks/user_svc_db.go
@@ -0,0 +1,34 @@
+package mocks
+
+import (
+	"github.com/angelhack2019/lib/models"
+)
+
+type Users []models.User
+
+var users = Users{
+	{
+		UUID:       "f30bf3a-8591-4034-950b-d611d3a93c47",
+		Email:      "jdoe@uw.edu",
+		Password:   "Unencrypted",
+		SumRatings: 11,
+		NumRatings: 3,
+		Bio:        "I am generous",
+		School:     "UW Bothel",
+		State:      "WA",
+		Phone:      "2061234567",
+		PicURL:     "https://f4.bcbits.com/img/a0777435656_10.jpg",
+	},
+	{
+		UUID:       "f30bf3a-8591-4034-950b-d611d3a93c47",
+		Email:      "sfields@uw.edu",
+		Password:   "NotSafe",
+		SumRatings: 0,
+		NumRatings: 0,
+		Bio:        "I like food",
+		School:     "UW Bothel",
+		State:      "WA",
+		Phone:      "2067654321",
+		PicURL:     "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Gidget_main_cast_1966_%28cropped%29.jpg/220px-Gidget_main_cast_1966_%28cropped%29.jpg",
+	},
+}
