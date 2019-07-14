bd488a0019437d76cdd049cff508159a162c1bf2
CJ Faraon <faraonc@outlook.com>
2019-07-13 18:56:20 -0700

    Add no error response

--- utility/util.go
+++ utility/util.go
@@ -9,6 +9,10 @@ func RespondWithError(w http.ResponseWriter, code int, message string) {
 	RespondWithJSON(w, code, map[string]string{"error": message})
 }
 
+func Respond(w http.ResponseWriter, code int, message string) {
+	RespondWithJSON(w, code, map[string]string{"response": message})
+}
+
 func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, err := json.Marshal(payload)
 
