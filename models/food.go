37c244e65054d71949b5dd7e6900b117cd78c6f8
Jacob Landowski <j.landowski@f5.com>
2019-07-14 02:09:27 -0700

    Quote json marshals in structs

--- models/food.go
+++ models/food.go
@@ -2,8 +2,8 @@ package models
 
 // Food Model
 type Food struct {
-	UUID       string `json:uuid`
-	PicURL     string `json:pic_url`
-	ExpDate    int64  `json:exp_date`
-	CreateDate int64  `json:create_date`
+	UUID       string `json:"uuid"`
+	PicURL     string `json:"pic_url"`
+	ExpDate    int64  `json:"exp_date"`
+	CreateDate int64  `json:"create_date"`
 }
--- models/user.go
+++ models/user.go
@@ -2,17 +2,17 @@ package models
 
 // User Model
 type User struct {
-	UUID        string `json:uuid`
-	FirstName	string `json:first_name`
-	LastName	string `json:last_name`
-	Email       string `json:email`
-	Password    string `json:password,omitempty`
-	SumRatings  int    `json:sum_ratings`
-	NumRatings  int    `json:num_ratings`
-	Bio         string `json:bio`
-	School      string `json:school`
-	State       string `json:state`
-	City        string `json:city`
-	PhoneNumber string `json:phone_number`
-	PicURL      string `json:pic_url`
+	UUID        string `json:"uuid"`
+	FirstName	string `json:"first_name"`
+	LastName	string `json:"last_name"`
+	Email       string `json:"email"`
+	Password    string `json:"password",omitempty`
+	SumRatings  int    `json:"sum_ratings"`
+	NumRatings  int    `json:"num_ratings"`
+	Bio         string `json:"bio"`
+	School      string `json:"school"`
+	State       string `json:"state"`
+	City        string `json:"city"`
+	PhoneNumber string `json:"phone_number"`
+	PicURL      string `json:"pic_url"`
 }
