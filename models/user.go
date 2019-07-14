8f1fdbdf9558ee3f2cffab47f0560bd1e32fc3e3
Jacob Landowski <j.landowski@f5.com>
2019-07-14 01:33:48 -0700

    Change Phone field to PhoneNumber in User struct

--- models/user.go
+++ models/user.go
@@ -2,15 +2,15 @@ package models
 
 // User Model
 type User struct {
-	UUID       string `json:uuid`
-	Email      string `json:email`
-	Password   string `json:password`
-	SumRatings int    `json:sum_ratings`
-	NumRatings int    `json:num_ratings`
-	Bio        string `json:bio`
-	School     string `json:school`
-	State      string `json:state`
-	City 	   string `json:city`
-	Phone      string `json:phone`
-	PicURL     string `json:pic_url`
+	UUID        string `json:uuid`
+	Email       string `json:email`
+	Password    string `json:password`
+	SumRatings  int    `json:sum_ratings`
+	NumRatings  int    `json:num_ratings`
+	Bio         string `json:bio`
+	School      string `json:school`
+	State       string `json:state`
+	City        string `json:city`
+	PhoneNumber string `json:phone_number`
+	PicURL      string `json:pic_url`
 }
