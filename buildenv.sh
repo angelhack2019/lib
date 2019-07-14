5283284b1bc1040f224720b1afc283e42ec0994a
CJ Faraon <faraonc@outlook.com>
2019-07-14 08:42:56 -0700

    Populate dummy data

---
+++ buildenv.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+
+# JOHN
+curl \
+    -F "exp_date=1563493799" \
+    -F "tags=john" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11" \
+    -F "image=@images/a1.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493899" \
+    -F "tags=john" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11" \
+    -F "image=@images/a2.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493799" \
+    -F "tags=john" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11" \
+    -F "image=@images/a3.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493899" \
+    -F "tags=john" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11" \
+    -F "image=@images/a4.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563494799" \
+    -F "tags=john" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11" \
+    -F "image=@images/a5.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493299" \
+    -F "tags=john" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11" \
+    -F "image=@images/a6.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493791" \
+    -F "tags=john" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11" \
+    -F "image=@images/a7.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493999" \
+    -F "tags=john" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11" \
+    -F "image=@images/a8.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563494799" \
+    -F "tags=john" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11" \
+    -F "image=@images/a9.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563495799" \
+    -F "tags=john" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11" \
+    -F "image=@images/a10.jpg" \
+    http://localhost:8081/food
+
+# SALLY
+
+curl \
+    -F "exp_date=1563493699" \
+    -F "tags=sally" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12" \
+    -F "image=@images/b1.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563893799" \
+    -F "tags=sally" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12" \
+    -F "image=@images/b2.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563494799" \
+    -F "tags=sally" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12" \
+    -F "image=@images/b3.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493899" \
+    -F "tags=sally" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12" \
+    -F "image=@images/b4.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493499" \
+    -F "tags=sally" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12" \
+    -F "image=@images/b5.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563423799" \
+    -F "tags=sally" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12" \
+    -F "image=@images/b6.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493599" \
+    -F "tags=sally" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12" \
+    -F "image=@images/b7.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493759" \
+    -F "tags=sally" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12" \
+    -F "image=@images/b8.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493799" \
+    -F "tags=sally" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12" \
+    -F "image=@images/b9.jpg" \
+    http://localhost:8081/food
+
+curl \
+    -F "exp_date=1563493729" \
+    -F "tags=sally" \
+    -F "user_uuid=a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12" \
+    -F "image=@images/b10.jpg" \
+    http://localhost:8081/food
